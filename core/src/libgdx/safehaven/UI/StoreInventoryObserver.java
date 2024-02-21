38a600c3185874c1195e7ff92bc5f120361aac2d
qbb84 <luke.wilder2000@gmail.com>
2024-02-21 20:33:28 +0000

    Fixed gradle:run error. Update packages & gradle version

---
+++ core/src/libgdx/safehaven/UI/StoreInventoryObserver.java
@@ -0,0 +1,10 @@
+package libgdx.safehaven.UI;
+
+public interface StoreInventoryObserver {
+	enum StoreInventoryEvent {
+        PLAYER_GP_TOTAL_UPDATED,
+        PLAYER_INVENTORY_UPDATED
+    }
+
+    void onNotify(String value, StoreInventoryEvent event);
+}
